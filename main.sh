c480a35d3b9dca407210bf95151685e058a6b295
Martin Chacon <chacon0754@outlook.com>
2025-04-14 12:42:26 -0600

    Changes in submenu

--- main.sh
+++ main.sh
@@ -15,41 +15,52 @@ submenu(){
         case $REPLY in
             1)
                 echo "Add info to ${method}"
+                read -p "Enter an identifier: " identifier
+                read -p "Enter the concept: " concept
+                echo "identifier: ${identifier}"
+                echo "concept: ${concept}" 
                 ;;
             2) 
                 echo "Search info in ${method}"
+                read -p "Enter the identifier to search: " searchID
+                echo "Searching ${searchID}"
                 ;;
             3)
                 echo "Delete concept in ${method}"
+                read -p "Enter the identifier to delet: " deleteID
+                echo "Searching ${deleteID}"
                 ;;
             4)
                 echo "read base of info in ${method}"
                 ;;
             5)
                 echo "Back to main menu"
+                return
                 ;;
             6)
-                echo "Byeeeee"
+                echo "Thank you for choosing us"
+                echo "Have a good day"
                 exit 0
                 ;;
             *) 
                 echo "Invalid option"
                 ;;
         esac
+        echo -e "\nPress Enter to continue"
+        read
+        echo ""
+        echo "You are in ${method} section, please select an option:"
     done
-
 
 }
 
 agileMethods=("SCRUM" "XP" "Kanban" "Crystal" "EXIT")
 traditionalMethods=("Cascade" "Spiral" "V Model" "EXIT")
-
 if [ "$1" == "-a" ]; then
     echo "Welcome to the Agile methodologies fast guide, to continue please choose a theme:"
     select method in "${agileMethods[@]}"; do
         case $REPLY in
             1|2|3|4)
-                echo "You choosed ${method}"
                 submenu "$method"
                 ;;
             5)
@@ -62,6 +73,9 @@ if [ "$1" == "-a" ]; then
                 echo "Please choose an option from 1 to 5"
                 ;;
         esac
+        echo -e "\nTo continue please Press ENTER"
+        read
+        echo "Welcome to the Agile methodologies fast guide, to continue please choose a theme:"
     done
 fi
 
@@ -70,7 +84,6 @@ if [ "$1" == "-t" ]; then
     select method in "${traditionalMethods[@]}"; do
         case $REPLY in
             1|2|3)
-                echo "You choosed ${method}"
                 submenu "$method"
                 ;;
             4)
@@ -82,5 +95,8 @@ if [ "$1" == "-t" ]; then
                 echo "Please try again"
                 ;;
         esac
+        echo -e "\nTo continue please Press ENTER"
+        read
+        echo "Welcome to the Traditional methodologies fast guide, to continue please choose a theme:"
     done
 fi
