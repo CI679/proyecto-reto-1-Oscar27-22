19b00ae4d73c37d98d2639232b67a34ab75e085c
Martin Chacon <chacon0754@outlook.com>
2025-04-14 11:14:38 -0600

    Add list of methods and manu for agile

--- main.sh
+++ main.sh
@@ -6,3 +6,25 @@ if [ "$1" != "-a" ] && [ "$1" != "-t" ]; then
     echo "-t for traditional"
     exit 1
 fi
+
+agileMethods=("SCRUM" "XP" "Kanban" "Crystal" "EXIT")
+traditionalMethods=("Cascade" "Spiral" "V Model" "EXIT")
+
+if [ "$1" == "-a" ]; then
+    select method in "${agileMethods[@]}"; do
+        case $REPLY in
+            1|2|3|4)
+                echo "You choosed ${method}"
+                ;;
+            5)
+                echo "Thank you for choosing us"
+                echo "Bye"
+                exit 0
+                ;;
+            *)
+                echo "Invalid option"
+                echo "Please choose an option from 1 to 5"
+                ;;
+        esac
+    done
+fi
