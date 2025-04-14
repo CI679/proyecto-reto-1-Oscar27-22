473e334f8e9d394828ec5fde01a86937999ff008
Martin Chacon <chacon0754@outlook.com>
2025-04-14 10:55:35 -0600

    Add first validation to main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+if [ "$1" != "-a" ] && [ "$1" != "-t" ]; then
+    echo "To use the program $0 you must give the following flags:"
+    echo "-a for agile"
+    echo "-t for traditional"
+    exit 1
+fi
